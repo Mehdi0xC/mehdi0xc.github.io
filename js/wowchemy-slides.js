22171ea6642abf58a95b96afa0403a95eb698777
Mehdi Safaee <mehdi@Mehdis-MacBook-Pro.local>
2022-05-17 21:56:44 -0700

    Update

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/var/folders/hn/hlrmzv3d11nggtm5n1szp22w0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210606130713-db4dcf6541e5/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/dz/q3m6qpzn72955pq8t6fjc3lw0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210606130713-db4dcf6541e5/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
